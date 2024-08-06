9cd4d160508d346d3c5af900fb2ee7f22745c230
rcmiryad123 <rcmiryad123@gmail.com>
2024-08-06 20:47:25 +0700

    ganti menyerah ke #

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const maxsimum = parseInt(prompt("Masukan Nilai Maksimum"));
 const targetNum = Math.floor(Math.random() * maxsimum) + 1;
 
+console.log(targetNum);
+
 let guess = prompt("Masukan Nilai Tebakan Pertama Anda");
 let attempts = 1;
 
-while (guess.toLocaleLowerCase != targetNum) {
-    if (guess == "menyerah") {
+while (guess != targetNum) {
+    if (guess == "#") {
         break;        
     }else {
         attempts++;
         if (guess > targetNum) {
-            guess = prompt("Nilai Tebakan Terlalu Tinggi, ketik 'menyerah' untuk menyerah");
+            guess = prompt("Nilai Tebakan Terlalu Tinggi, ketik '#' untuk menyerah");
         } else {
-            guess = prompt("Nilai Tebakan Terlalu Rendah, ketik 'menyerah' untuk menyerah");
+            guess = prompt("Nilai Tebakan Terlalu Rendah, ketik '#' untuk menyerah");
         }
     }
 }
 
-if (guess == "menyerah") {
+if (guess == "#") {
     alert("Anda Menyerah, sayang sekali.");
 }else {
     alert(`Selamat! Anda menebak dengan benar. Nilai target adalah ${targetNum} & Anda menebak ${attempts} kali.`);
